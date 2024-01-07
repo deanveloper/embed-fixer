cd7e1cd840567316b86e3ee0ffe34970db640221
dean <deanvelopermn@gmail.com>
2024-01-07 15:44:55 -0800

    suppress embeds

--- main.go
+++ main.go
@@ -89,6 +89,7 @@ func suppressEmbeds(s *discordgo.Session, msg *discordgo.Message) error {
 
 	channelMessageEndpoint := discordgo.EndpointChannelMessage(msg.ChannelID, msg.ID)
 	response, err := s.Request("PATCH", channelMessageEndpoint, flags)
+	log.Printf("original message: %+v\n", msg)
 	log.Printf("request data: %+v\n", flags)
 	log.Printf("response: %s\n", response)
 	return err
